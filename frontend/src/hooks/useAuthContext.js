93a71fbddf73d0c74a0e51d98b4c242fdf45be12
RAJ PATEL <patelraj24365@gmail.com>
2023-10-26 15:01:54 -0400

    signup and logout hook created

--- frontend/src/hooks/useAuthContext.js
+++ frontend/src/hooks/useAuthContext.js
@@ -1,8 +1,8 @@
-import { AuthContext } from "../context/WorkoutContext";
+import { AuthContext } from "../context/AuthContext";
 import { useContext } from "react";
 
 export const useAuthContext = () => {
-    const context = useContext(Authcontext)
+    const context = useContext(AuthContext)
 
     if (!context){
         throw Error('useAuthContext must be used inside a AuthContextProvider')
