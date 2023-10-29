61dddc1f144110f60599dc9fb35003d585ecda44
RAJ PATEL <patelraj24365@gmail.com>
2023-10-29 16:44:09 -0400

    deployment changes

--- frontend/src/components/WorkoutForm.js
+++ frontend/src/components/WorkoutForm.js
@@ -20,7 +20,7 @@ const WorkoutForm = () => {
 
     const workout = {title, load, reps}
     
-    const response = await fetch('/api/workouts', {
+    const response = await fetch('https://workout-tracker-ruby.vercel.app/api/workouts', {
       method: 'POST',
       body: JSON.stringify(workout),
       headers: {
